93a2ac4cde6c346bd574140b742d87ca3f2b2ed4
Surajit <piklu3043@gmail.com>
2022-01-16 23:55:57 +0530

    This is now fully functional Game

--- main.js
+++ main.js
@@ -1,26 +1,57 @@
 var Question = function (question, answers, correctAnswer) {
     this.question = question;
-    this.option_a = answers[0];
-    this.option_b = answers[1];
-    this.option_c = answers[2];
+    this.answersObject = {
+        'a': answers[0],
+        'b': answers[1],
+        'c': answers[2]
+    }
     this.correctAnswer = correctAnswer;
 };
 
+var score = 0;
+
 var question1 = new Question('What is my fevourite programmming language ?', ['JavaScript', 'Ruby', 'Java'], 'a');
+var question2 = new Question('Which one of this is my fevourite food ?', ['Pasta', 'Burger', 'Ramen'], 'c');
+var question3 = new Question('What can I drink for whole day ?', ['Beer', 'Coffe', 'Milk Shake'], 'b');
+var question4 = new Question('My favourite thing to do ?', ['Playing video games', 'Chating with Friends', 'Watching Animes'], 'c');
+var question5 = new Question('What is my DREAM ?', ['Developer', 'Entrapenaur', 'Freelancer'], 'c');
 
-console.log('Question : '+question1.question);
-console.log('a : ' + question1.option_a);
-console.log('b : ' + question1.option_b);
-console.log('c : ' + question1.option_c);
+var questions = [question1, question2, question3, question4, question5];
 
-var answer = prompt('Write the answer : ');
+function gameLoop() {
+    var randomQuestion = questions[Math.floor(Math.random() * 5)];
+    console.log('Your Score is : ' + score);
 
-if (answer == question1.correctAnswer) {
-    alert('You Win!')
-} else {
-    alert('You Lose :(')
+    console.log('Question : ' + randomQuestion.question);
+    console.log('a : ' + randomQuestion.answersObject.a);
+    console.log('b : ' + randomQuestion.answersObject.b);
+    console.log('c : ' + randomQuestion.answersObject.c);
+    
+
+    var answer = prompt('Enter the CORRECT option : ');
+    
+    
+    if (answer === randomQuestion.correctAnswer) {
+        alert('You Win :)');
+        score += 1;
+        console.log(`You choose the CORRECT answer.`);
+    } else {
+        alert('You Lose :(')
+        console.log(`You choose the WRONG answer.`);
+    }
+    
+    console.clear();
+    
+    if (answer === 'exit') {
+        console.log('Game Over!');
+    } else {
+        gameLoop();
+    }
 }
 
+// gameLoop();
+
+document.getElementById('start-btn').addEventListener('click', gameLoop);
 
 
 
